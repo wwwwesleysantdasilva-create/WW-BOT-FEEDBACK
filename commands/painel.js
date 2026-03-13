92bd3a9786577793ea892ee03dde5b77f55bfaf6
wwwwesleysantdasilva-create <wwwwesleysantdasilva@gmail.com>
2026-03-13 18:34:07 -0300

    Update painel.js

--- commands/painel.js
+++ commands/painel.js
@@ -1,1 +1,34 @@
+import { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } from "discord.js"
 
+export const data = {
+ name: "painel",
+ description: "Abrir painel do bot"
+}
+
+export async function execute(interaction){
+
+ const embed = new EmbedBuilder()
+ .setTitle("⚙️ Painel do Bot de Feedback")
+ .setDescription("Use os botões abaixo para configurar o sistema.")
+ .setColor("Blue")
+
+ const row = new ActionRowBuilder().addComponents(
+
+  new ButtonBuilder()
+   .setCustomId("set_feedback")
+   .setLabel("Configurar Canal")
+   .setStyle(ButtonStyle.Primary),
+
+  new ButtonBuilder()
+   .setCustomId("send_embed")
+   .setLabel("Enviar Embed Feedback")
+   .setStyle(ButtonStyle.Success)
+
+ )
+
+ await interaction.reply({
+  embeds:[embed],
+  components:[row]
+ })
+
+}
