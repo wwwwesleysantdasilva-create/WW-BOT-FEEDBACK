2e9636cb74cabe4108f65f3ceb2477df5d4e17a1
wwwwesleysantdasilva-create <wwwwesleysantdasilva@gmail.com>
2026-03-13 17:58:21 -0300

    Initialize Discord client and load events

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+import { Client, GatewayIntentBits } from "discord.js"
+import fs from "fs"
 
+const client = new Client({
+ intents: [
+  GatewayIntentBits.Guilds,
+  GatewayIntentBits.GuildMessages,
+  GatewayIntentBits.MessageContent
+ ]
+})
+
+/* CARREGAR EVENTS */
+
+fs.readdirSync("./events").forEach(file=>{
+ import(`./events/${file}`).then(event=>{
+  event.default(client)
+ })
+})
+
+client.login(process.env.TOKEN)
