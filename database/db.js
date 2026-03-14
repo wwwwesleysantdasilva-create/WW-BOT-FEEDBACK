74c400f821816f5f8afd6e63b3e23ad53299e00a
wwwwesleysantdasilva-create <wwwwesleysantdasilva@gmail.com>
2026-03-13 21:55:46 -0300

    Atualizar o db.js

--- database/db.js
+++ database/db.js
@@ -6,8 +6,15 @@ db.serialize(()=>{
 
  db.run(`
  CREATE TABLE IF NOT EXISTS config(
- guild TEXT,
- feedbackChannel TEXT
+ guild TEXT PRIMARY KEY,
+ feedbackChannel TEXT,
+ embedTitle TEXT,
+ embedDescription TEXT,
+ embedImage TEXT,
+ embedColor TEXT,
+ buttonLabel TEXT,
+ buttonEmoji TEXT,
+ buttonStyle TEXT
  )
  `)
 
