3b5f4779ed2d262bc97f5e55be732bdcb2b90270
wwwwesleysantdasilva-create <wwwwesleysantdasilva@gmail.com>
2026-03-13 22:13:49 -0300

    Atualizar o messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -3,13 +3,105 @@ import db from "../database/db.js"
 
 export default (client)=>{
 
- client.on("messageCreate",async msg=>{
+ client.on("messageCreate", async msg=>{
 
   if(msg.author.bot) return
 
-  const step = client.feedbackStep[msg.author.id]
+  /* ================= EDITAR TEXTO EMBED ================= */
 
-  /* PASSO 1 - MIDIA */
+  if(client.editEmbedUser === msg.author.id){
+
+   const data = msg.content.split("|")
+
+   const title = data[0]?.trim()
+   const desc = data[1]?.trim()
+
+   db.run(`
+   INSERT OR REPLACE INTO config
+   (guild, embedTitle, embedDescription)
+   VALUES (?,?,?)
+   `,
+   [
+    msg.guild.id,
+    title,
+    desc
+   ])
+
+   msg.reply("✅ Texto da embed atualizado!")
+
+   client.editEmbedUser = null
+   return
+  }
+
+  /* ================= EDITAR IMAGEM ================= */
+
+  if(client.editImageUser === msg.author.id){
+
+   db.run(`
+   INSERT OR REPLACE INTO config
+   (guild, embedImage)
+   VALUES (?,?)
+   `,
+   [
+    msg.guild.id,
+    msg.content
+   ])
+
+   msg.reply("✅ Imagem da embed atualizada!")
+
+   client.editImageUser = null
+   return
+  }
+
+  /* ================= EDITAR COR ================= */
+
+  if(client.editColorUser === msg.author.id){
+
+   db.run(`
+   INSERT OR REPLACE INTO config
+   (guild, embedColor)
+   VALUES (?,?)
+   `,
+   [
+    msg.guild.id,
+    msg.content
+   ])
+
+   msg.reply("✅ Cor da embed atualizada!")
+
+   client.editColorUser = null
+   return
+  }
+
+  /* ================= EDITAR BOTÃO ================= */
+
+  if(client.editButtonUser === msg.author.id){
+
+   const data = msg.content.split("|")
+
+   const emoji = data[0]?.trim()
+   const label = data[1]?.trim()
+
+   db.run(`
+   INSERT OR REPLACE INTO config
+   (guild, buttonEmoji, buttonLabel)
+   VALUES (?,?,?)
+   `,
+   [
+    msg.guild.id,
+    emoji,
+    label
+   ])
+
+   msg.reply("✅ Botão atualizado!")
+
+   client.editButtonUser = null
+   return
+  }
+
+  /* ================= SISTEMA DE FEEDBACK ================= */
+
+  const step = client.feedbackStep[msg.author.id]
 
   if(step === "media"){
 
@@ -21,10 +113,7 @@ export default (client)=>{
    client.feedbackStep[msg.author.id] = "text"
 
    return msg.reply("✍️ Agora escreva seu feedback.")
-
   }
-
-  /* PASSO 2 - TEXTO */
 
   if(step === "text"){
 
